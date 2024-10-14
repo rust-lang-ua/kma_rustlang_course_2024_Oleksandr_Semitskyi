54df8fc4847dee07247fbc51bf3c255c7d5b1e20
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-10-14 21:26:03 +0300

    Done if, move semantic and quiz modules

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -8,7 +8,7 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
+// I AM DONE
 
 // Put your function here!
 // fn calculate_apple_price {
@@ -23,4 +23,12 @@ fn verify_test() {
     assert_eq!(70, price1);
     assert_eq!(80, price2);
     assert_eq!(65, price3);
+}
+
+fn calculate_apple_price(count: u32) -> u32 {
+    if count > 40 {
+        count
+    } else {
+        count * 2
+    }
 }
